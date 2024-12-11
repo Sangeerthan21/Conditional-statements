65d91fe17955ce18d6ead70fe5ae1f0af1abc316
Sangeerthan21 <kandeepann5@gmail.com>
2024-12-11 18:51:45 +0530

    Conditional Statements

---
+++ activity3.py
@@ -0,0 +1,8 @@
+i = float(input("Enter a number: "))
+if (i < 15):
+    print ("i is smaller than 15")
+    print ("I'm in if Block")
+else:
+    print ("i is greater than 15")
+    print ("I'm in else Block")
+print ("I am not in if block or else block")    
